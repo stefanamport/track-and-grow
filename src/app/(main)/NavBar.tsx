1c39459fa2498deead644e08265402926da309dc
Stefan Amport <stefanamport@macbook-pro-von-stefan.home>
2025-04-23 09:14:50 +0200

    rebranding

--- src/app/(main)/NavBar.tsx
+++ src/app/(main)/NavBar.tsx
@@ -100,7 +100,7 @@ export function NavBar() {
         <Icon size="lg">
           <Icons.Logo />
         </Icon>
-        <Text>umami</Text>
+        <Text>TrackGrow</Text>
       </div>
       <div className={styles.links}>
         {links.map(({ url, label }) => {
